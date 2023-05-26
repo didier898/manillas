70191bceb2ca2cbcb672f7de28ae37e411eae57c
Didier <didier002010@gmail.com>
2023-05-26 18:54:17 -0500

    seleccionar cantidad y material + titulo

--- src/components/Manillas.jsx
+++ src/components/Manillas.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Manillas = () => {
+    const [cantidad, setCantidad] = useState(0);
+    const [material, setMaterial] = useState('cuero');
+
+    const manejarCambioCantidad = (event) => {
+        setCantidad(parseInt(event.target.value, 10));
+      };
+
+      const manejarCambioMaterial = (event) => {
+        setMaterial(event.target.value);
+      };
+
+
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();    } 
   return (
-    <div>Manillas</div>
+    <div className='container mt-5'>
+      <h1 className='text-center'> Manillas didier </h1>
+      <hr />
+      <div className='row'>
+        <div className='col-8'></div>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Cantidad:
+            <input type='number' value={cantidad} onChange={manejarCambioCantidad} />
+          </label>
+          <br />
+          <label>
+            Material:
+            <select value={material} onChange={manejarCambioMaterial}>
+              <option value='cuero'>Cuero</option>
+              <option value='cuerda'>Cuerda</option>
+            </select>
+          </label>
+          </form>
+    </div>
+    </div>
   )
 }
 
