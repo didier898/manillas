24ad917ce0dc66ce24ccb5d9ce66460cc2ce8285
Didier <didier002010@gmail.com>
2023-05-26 19:50:42 -0500

    costo según la cantidad y cálculo de combinaciones

--- src/components/Manillas.jsx
+++ src/components/Manillas.jsx
@@ -6,36 +6,91 @@ const Manillas = () => {
     const [dije, setDije] = useState('martillo');
     const [moneda, setMoneda] = useState('dolar');
     const [tipo, setTipo] = useState('oro');
+    const [costo, setCosto] = useState(0);
     
-
     const manejarCambioCantidad = (event) => {
         setCantidad(parseInt(event.target.value, 10));
       };
-
+      
       const manejarCambioMaterial = (event) => {
         setMaterial(event.target.value);
       };
       const manejarCambioDije = (event) => {
         setDije(event.target.value);
       };
-
+    
       const manejarCambioMoneda = (event) => {
         setMoneda(event.target.value);
       };
-
+    
       const manejarCambioTipo = (event) => {
         setTipo(event.target.value);
       };
-
-      
 
 
 
 
     const handleSubmit = (event) => {
-        event.preventDefault();    } 
+        event.preventDefault();      
+    
+    //  cálculos de las combinaciones 
+    let nuevoCosto = 0;
+    if (material === 'cuero') {
+      if (dije === 'martillo') {
+        if (tipo === 'oro' || tipo === 'ororosado') {
+          nuevoCosto = 100;
+        } else if (tipo === 'plata') {
+          nuevoCosto = 80;
+        } else if (tipo === 'niquel') {
+          nuevoCosto = 70;
+        }
+      } else if (dije === 'ancla') {
+        if (tipo === 'oro' || tipo === 'ororosado') {
+          nuevoCosto = 120;
+        } else if (tipo === 'plata') {
+          nuevoCosto = 100;
+        } else if (tipo === 'niquel') {
+          nuevoCosto = 90;
+        }
+      }
+    } else if (material === 'cuerda') {
+      if (dije === 'martillo') {
+        if (tipo === 'oro' || tipo === 'ororosado') {
+          nuevoCosto = 90;
+        } else if (tipo === 'plata') {
+          nuevoCosto = 70;
+        } else if (tipo === 'niquel') {
+          nuevoCosto = 50;
+        }
+      } else if (dije === 'ancla') {
+        if (tipo === 'oro' || tipo === 'ororosado') {
+          nuevoCosto = 110;
+        } else if (tipo === 'plata') {
+          nuevoCosto = 90;
+        } else if (tipo === 'niquel') {
+          nuevoCosto = 80;
+        }
+      }
+      
+    }
+    //costo a la moneda seleccionada
+    if (moneda === 'pesos') {
+        nuevoCosto *= 5000; // Conversión de dólares a pesos (1 dólar = 5000 pesos)
+      }
+  
+      // costo según la cantidad
+      nuevoCosto *= cantidad;
+  
+      // Actualizar el estado del costo
+      setCosto(nuevoCosto);
+    };
+    
+      
+        
+
   return (
-    <div className='container mt-5'>
+    
+        <div className='container mt-5'>
       <h1 className='text-center'> Manillas didier </h1>
       <hr />
       <div className='row'>
@@ -55,6 +110,7 @@ const Manillas = () => {
               <option value='cuerda'>Cuerda</option>
             </select>
           </label>
+          <br />
           <label>
             Dije:
             <select value={dije} onChange={manejarCambioDije}>
@@ -82,12 +138,10 @@ const Manillas = () => {
           </label>
           <br />
           <button type='submit'>Calcular</button>
-
-
-
           </form>
     </div>
     </div>
+    
   )
 }
 
