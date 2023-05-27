0a68ecdc28c4c195e5edc79970c719fb7b7d527c
Didier <didier002010@gmail.com>
2023-05-26 19:25:31 -0500

    botón calcular añadido, moneda,dije,tipo

--- src/components/Manillas.jsx
+++ src/components/Manillas.jsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 const Manillas = () => {
     const [cantidad, setCantidad] = useState(0);
     const [material, setMaterial] = useState('cuero');
+    const [dije, setDije] = useState('martillo');
+    const [moneda, setMoneda] = useState('dolar');
+    const [tipo, setTipo] = useState('oro');
+    
 
     const manejarCambioCantidad = (event) => {
         setCantidad(parseInt(event.target.value, 10));
@@ -11,6 +15,19 @@ const Manillas = () => {
       const manejarCambioMaterial = (event) => {
         setMaterial(event.target.value);
       };
+      const manejarCambioDije = (event) => {
+        setDije(event.target.value);
+      };
+
+      const manejarCambioMoneda = (event) => {
+        setMoneda(event.target.value);
+      };
+
+      const manejarCambioTipo = (event) => {
+        setTipo(event.target.value);
+      };
+
+      
 
 
 
@@ -38,6 +55,36 @@ const Manillas = () => {
               <option value='cuerda'>Cuerda</option>
             </select>
           </label>
+          <label>
+            Dije:
+            <select value={dije} onChange={manejarCambioDije}>
+              <option value='martillo'>Martillo</option>
+              <option value='ancla'>Ancla</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Tipo:
+            <select value={tipo} onChange={manejarCambioTipo}>
+              <option value='oro'>Oro</option>
+              <option value='ororosado'>Oro Rosado</option>
+              <option value='plata'>Plata</option>
+              <option value='niquel'>Niquel</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Moneda:
+            <select value={moneda} onChange={manejarCambioMoneda}>
+              <option value='dolar'>Dólar</option>
+              <option value='pesos'>Pesos</option>
+            </select>
+          </label>
+          <br />
+          <button type='submit'>Calcular</button>
+
+
+
           </form>
     </div>
     </div>
